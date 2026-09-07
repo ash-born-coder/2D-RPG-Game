69cc7f403e677bd3bb9a7a4c97e116c79f5ccbdd
Chanuka Ashan Gunawardena <chanukagunawardena63@gmail.com>
2026-09-07 13:19:54 +0200

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rb;
+
+    [Header("Movement details")]
+    [SerializeField] private float moveSpeed = 3.5f;
+    [SerializeField] private float jumpForce = 8;
+    private float xInput;
+    private bool facingRight = true;
+
+    [Header("Collision details")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+
+
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        HandleCollision();
+        HandleInput();
+        HandleMovement();
+        HandleAnimations();
+        HandleFlip();
+
+    }
+
+    private void HandleAnimations()
+    {
+        anim.SetFloat("xVelocity", rb.linearVelocity.x);
+        anim.SetFloat("yVelocity", rb.linearVelocity.y);
+        anim.SetBool("isGrounded", isGrounded);
+    }
+
+    private void HandleInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void HandleMovement()
+    {
+        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    private void HandleCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    private void HandleFlip()
+    {
+        if (rb.linearVelocity.x > 0 && facingRight == false)
+        {
+            Flip();
+        }
+        else if (rb.linearVelocity.x < 0 && facingRight == true)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(0, -groundCheckDistance));
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# RPG New
+A 2D RPG game built with Unity and C# as a personal project to explore game development, gameplay mechanics, animation, and object-oriented programming. Currently in active development.
